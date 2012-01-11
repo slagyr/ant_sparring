c5cb22f6c197174ced61ede70e4987576db6d5b6
Micah <micah@8thlight.com>
2012-01-10 21:13:06 -0600

    added food

--- engine/spec/ants/engine/core_spec.clj
+++ engine/spec/ants/engine/core_spec.clj
@@ -36,47 +36,111 @@
       (should= 2 (count @stuff))
       (should= ["Phil has entered the world!"] results)
       (let [ant (get @stuff id)]
+        (should= :ant (:type ant))
         (should= "Phil" (:name ant))
         (should= [0 0] (:location ant)))))
 
   (context "with one ant"
 
-    (with ant (join @world "George"))
-    (before (do @ant (tick @world)))
+    (with ant-id (join @world "George"))
+    (before (do @ant-id (tick @world)))
 
     (it "can give go command"
-      (go @world @ant "north")
-      (let [command (get @commands @ant)]
+      (go @world @ant-id "north")
+      (let [command (get @commands @ant-id)]
         (should-not= nil command)
+        (should= :go (:command command))
         (should= "north" (:direction command))
-        (should= @ant (:id command))))
+        (should= @ant-id (:id command))))
 
     (it "ant can go north"
-      (go @world @ant "north")
+      (go @world @ant-id "north")
       (let [results (tick @world)]
         (should= ["George went north"] results))
-      (should= [0 -1] (:location (get @stuff @ant))))
+      (should= [0 -1] (:location (get @stuff @ant-id))))
 
     (it "ant can go south"
-      (go @world @ant "south")
+      (go @world @ant-id "south")
       (let [results (tick @world)]
         (should= ["George went south"] results))
-      (should= [0 1] (:location (get @stuff @ant))))
+      (should= [0 1] (:location (get @stuff @ant-id))))
 
     (it "ant can go east"
-      (go @world @ant "east")
+      (go @world @ant-id "east")
       (let [results (tick @world)]
         (should= ["George went east"] results))
-      (should= [1 0] (:location (get @stuff @ant))))
+      (should= [1 0] (:location (get @stuff @ant-id))))
 
     (it "ant can go west"
-      (go @world @ant "west")
+      (go @world @ant-id "west")
       (let [results (tick @world)]
         (should= ["George went west"] results))
-      (should= [-1 0] (:location (get @stuff @ant))))
+      (should= [-1 0] (:location (get @stuff @ant-id))))
 
+    (it "can't add command for existing id"
+      (go @world @ant-id "west")
+      (should-throw java.lang.Exception "You're allowed only 1 command per tick"
+        (go @world @ant-id "north"))
+      (should= 1 (count @commands)))
+
+    (context "finding food"
+      (it "gets stored and award points"
+        (place-food @world [1 0])
+        (tick @world)
+        (go @world @ant-id "east")
+        (let [results (tick @world)
+              ant (get @stuff @ant-id)]
+          (should= true (:got-food ant))
+          (should= 1 (:points ant))
+          (should= ["George went east and found some FOOD! (1 point)"] results)))
+
+      (it "does nothing if it's already got food"
+        (place-food @world [1 0])
+        (place-food @world [1 1])
+        (tick @world)
+        (go @world @ant-id "east")
+        (tick @world)
+        (go @world @ant-id "north")
+        (let [results (tick @world)
+              ant (get @stuff @ant-id)]
+          (should= true (:got-food ant))
+          (should= 1 (:points ant))
+          (should= ["George went north"] results)))
+      )
     )
 
+  (context "food"
+
+    (it "can be placed"
+      (let [id (place-food @world [5 5])
+            command (get @commands id)]
+        (should-not= nil command)
+        (should= :place-food (:command command))
+        (should= [5 5] (:location command))
+        (should= id (:id command))
+        (should-not= nil (:timestamp command))))
+
+    (it "will appear"
+      (let [food-id (place-food @world [5 5])
+            results (tick @world)
+            food (get @stuff food-id)]
+        (should= :food (:type food))
+        (should= [5 5] (:location food))
+        (should= food-id (:id food))
+        (should= ["New food appeared at (5, 5)"] results)))
+
+    (it "can be removed"
+      (let [id (place-food @world [5 5])
+            _ (tick @world)
+            remove-id (remove-food @world [5 5])
+            command (get @commands remove-id)]
+        (should-not= nil command)
+        (should= id remove-id)
+        (should= :remove-food (:command command))
+        (should= [5 5] (:location command))
+        (should= id (:id command))
+        (should-not= nil (:timestamp command))))
+    )
   )
 
 (run-specs)
--- engine/src/ants/engine/core.clj
+++ engine/src/ants/engine/core.clj
@@ -7,6 +7,8 @@
     (ref {:nest {:location [0 0]}})
     (ref {})))
 
+; COMMAND GENERATION -----------------------------------------------------------------
+
 (defn- gen-id []
   (int (rand 100000000)))
 
@@ -14,6 +16,10 @@
   (or
     (some #(= name (:name %)) (vals @(.stuff world)))
     (some #(= name (:name %)) (vals @(.commands world)))))
+
+(defn- check-single-command [world id]
+  (when (get @(.commands world) id)
+    (throw (Exception. "You're allowed only 1 command per tick"))))
 
 (defn join [world name]
   (dosync
@@ -25,10 +31,28 @@
 
 (defn go [world ant direction]
   (dosync
+    (check-single-command world ant)
     (alter (.commands world) assoc ant {:command :go :id ant :direction direction :timestamp (System/nanoTime)})))
 
+(defn place-food [world location]
+  (dosync
+    (let [id (gen-id)]
+      (alter (.commands world) assoc id {:command :place-food :location location :id id :timestamp (System/nanoTime)})
+      id)))
+
+(defn remove-food [world location]
+  (dosync
+    (if-let [food (first (filter (fn [t] (and (= :food (:type t)) (= location (:location t)))) (vals @(.stuff world))))]
+      (do
+        (check-single-command world (:id food))
+        (alter (.commands world) assoc (:id food) {:command :remove-food :location location :id (:id food) :timestamp (System/nanoTime)})
+        (:id food))
+      (throw (Exception. (str "No food found at " location))))))
+
+; COMMAND EXECUTION -----------------------------------------------------------------
+
 (defn- do-join [stuff command]
-  (alter stuff assoc (:id command) {:name (:name command) :location [0 0]})
+  (alter stuff assoc (:id command) {:type :ant :name (:name command) :location [0 0]})
   (format "%s has entered the world!" (:name command)))
 
 (defn- new-location [[x y] dir]
@@ -38,19 +62,42 @@
     (= "east" dir) [(+ x 1) y]
     (= "west" dir) [(- x 1) y]))
 
+(defn- food-at? [stuff loc]
+  (some
+    #(and (= :food (:type %)) (= loc (:location %)))
+    (vals stuff)))
+
+(defn- award-points [ant points]
+  (let [value (or (:points ant) 0)]
+    (assoc ant :points (+ points value))))
+
+(defn- award-food [ant found?]
+  (if found?
+    (assoc (award-points ant 1) :got-food true)
+    ant))
+
 (defn- do-go [stuff command]
   (let [ant-id (:id command)
         ant (get @stuff ant-id)
-        current-loc (:location ant)
         dir (:direction command)
-        new-loc (new-location current-loc dir)]
-    (alter stuff assoc ant-id (assoc ant :location new-loc))
-    (format "%s went %s" (:name ant) dir)))
+        current-loc (:location ant)
+        new-loc (new-location current-loc dir)
+        found-food? (and (not (:got-food? ant)) (food-at? @stuff new-loc))
+        ant (award-food (assoc ant :location new-loc) found-food?)]
+    (alter stuff assoc ant-id ant)
+    (format "%s went %s%s" (:name ant) dir (if found-food? " and found some FOOD! (1 point)" ""))))
+
+(defn- do-place-food [stuff command]
+  (let [food-id (:id command)
+        [x y] (:location command)]
+    (alter stuff assoc food-id {:type :food :location [x y] :id food-id})
+    (format "New food appeared at (%s, %s)" x y)))
 
 (def command-map
   {
     :join do-join
     :go do-go
+    :place-food do-place-food
     })
 
 (defn execute-command [stuff command]
